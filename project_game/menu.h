d72efa301df41ef383add13d55acf12e3be9d804
William Tremblay <tremwil@gmail.com>
2020-03-09 16:18:24 -0400

    Finish menu

--- project_game/menu.h
+++ project_game/menu.h
@@ -1,12 +1,16 @@
 #ifndef _MENU_H
 #define MENU_H
 
-#define MENU_SUBMENU 0
-#define MENU_ACTION 1
-#define MENU_BOOL 2
-#define MENU_NUM 4
-#define MENU_EXIT 8
-#define MENU_READONLY 16
+#include <LiquidCrystal.h>
+
+#define MENU_SUBMENU 1
+#define MENU_EXIT 2
+#define MENU_ACTION 4
+#define MENU_BOOL 8
+#define MENU_NUM 16
+#define MENU_READONLY 32
+
+typedef void (*Action)();
 
 class MenuPage
 {
@@ -36,7 +40,7 @@ public:
 	}
    
     // Create action menu
-    MenuPage(const char* title, void (*action)())
+    MenuPage(const char* title, Action action)
     {
         this->title = title;
         this->parent = NULL;
@@ -46,7 +50,7 @@ public:
         
         this->type = MENU_ACTION;
         this->value = (int)action;
-        this->defVal = NULL;
+        this->defVal = (int)action;
     }
 
     // Create general menu.
@@ -86,6 +90,163 @@ public:
             }
         }
         return NULL;
+    }
+
+    // Return if the entire menu tree from this point is on default values.
+    bool isTreeDefault()
+    {
+        if (value != defVal) return false;
+
+        for (int i = 0; i < nChild; i++)
+        {
+            if (!children[i].isTreeDefault()) return false;
+        }
+
+        return true;
+    }
+
+    // Reset the menu to its default value (and all sub... menus if recurse is true).
+    void reset(bool recurse)
+    {
+        if (!(type & MENU_READONLY)) value = defVal;
+
+        if (recurse)
+        {
+            for (int i = 0; i < nChild; i++)
+            {
+                children[i].reset(true);
+            }   
+        }
+    }
+};
+
+// Handles menu navigation
+class MenuHandler
+{
+public:
+    LiquidCrystal* lcd;
+    MenuPage* currPage;
+    int subIndex;
+
+    // Create MenuHandler from LCD and MenuPage pointers
+    MenuHandler(LiquidCrystal* lcd, MenuPage* currPage)
+    {
+        this->lcd = lcd;
+        this->currPage = currPage;
+        this->subIndex = 0;
+    }
+
+    // Render current menu on the LCD.
+    void render()
+    {
+        lcd->clear();
+        lcd->setCursor(0,0);
+        lcd->print((currPage->parent == NULL) ? "  " : "^ ");
+        lcd->print(currPage->title);
+
+        if (currPage->type & MENU_SUBMENU)
+        {
+            lcd->setCursor(0,1);
+            lcd->print("< ");
+            lcd->print(currPage->children[subIndex].title);
+            lcd->setCursor(15, 1);
+            lcd->print(">");
+        }
+        else if (currPage->type & (MENU_NUM | MENU_BOOL))
+        {
+            lcd->setCursor(0,1);
+            if (currPage->type & MENU_NUM) lcd->print(currPage->value);
+            if (currPage->type & MENU_BOOL) lcd->print(currPage->value ? "True" : "False");
+
+            if (currPage->value == currPage->defVal) lcd->print(" (def)");
+        }
+    }
+
+    // Set the current page of this MenuHandler
+    void setPage(MenuPage *page)
+    {
+        currPage = page;
+        subIndex = 0;
+        render();
+    }
+
+    // Call when select button is pressed. Returns true if the menu should be closed.
+    bool onSelect()
+    {
+        if (currPage->type & MENU_SUBMENU)
+        {
+            if (currPage->children[subIndex].type & MENU_EXIT) return true;
+
+            if (currPage->children[subIndex].type & MENU_ACTION)
+            {
+               Action aptr = (Action)currPage->children[subIndex].value; aptr();
+            }
+            else
+            {
+                currPage = &currPage->children[subIndex];
+                subIndex = 0;   
+            }
+        }
+        if ((currPage->type & MENU_BOOL) && !(currPage->type & MENU_READONLY))
+        {
+            currPage->value = !currPage->value;
+        }
+
+        render();
+        return false;
+    }
+
+    // Call when escape button is pressed. Goes upward through the menu tree.
+    void onEscape()
+    {
+        if (currPage->parent != NULL)
+        {
+            currPage = currPage->parent;
+            subIndex = 0;
+            render();
+        }
+    }
+
+    // Call when reset button is pressed.
+    void onReset()
+    {
+        currPage->reset(true);
+        render();
+    }
+
+    // Call when moving through sub menus.
+    void onMoveSubMenu(int shift)
+    {
+        if (currPage->type & MENU_SUBMENU)
+        {
+            subIndex = (subIndex + shift) % currPage->nChild;
+            if (subIndex < 0) subIndex += currPage->nChild;
+            render();
+        }
+    }
+
+    // Call when delete button is pressed.
+    void onDelete()
+    {
+        if ((currPage->type & MENU_NUM) && !(currPage->type & MENU_READONLY))
+        {
+            currPage->value /= 10;
+            render();
+        }
+    }
+
+    // Call when digit button (0-9) is pressed.
+    void onDigit(int digit)
+    {
+        if ((currPage->type & MENU_NUM) && !(currPage->type & MENU_READONLY))
+        {
+            int new_val = 10*currPage->value + digit;
+            if (new_val < 0x8000) 
+            {
+                currPage->value = new_val;
+                render();
+            }
+        }
     }
 };
 
--- project_game/project_game.ino
+++ project_game/project_game.ino
@@ -9,27 +9,39 @@
 #define PIN_Y A2
 #define PIN_X A3
 
-int JOY_CX, JOY_CY;
+int JOY_CX = 512, JOY_CY = 512;
 void calibrate()
 {
     JOY_CX = analogRead(PIN_X);
     JOY_CY = analogRead(PIN_Y);
 }
 
-MenuPage menuRoot = MenuPage("- LCD GAME - ", 3, new MenuPage[3]
-{
-    MenuPage("Play", MENU_EXIT, 0),
-    MenuPage("High Score", MENU_NUM | MENU_READONLY, 0),
-    MenuPage("Settings", 0, NULL)
-});
-
 // (RS, E, D4, D5, D6, D7)
 LiquidCrystal lcd(5, 6, 7, 8, 9, 10);
 
+MenuPage menuRoot = MenuPage("- LCD GAME - ", 3, new MenuPage[3]
+{
+    MenuPage("Play", MENU_EXIT, 0),
+    MenuPage("Highscore", MENU_NUM | MENU_READONLY, 0),
+    MenuPage("Settings", 3, new MenuPage[3]
+    {
+        MenuPage("Calibrate", calibrate),
+        MenuPage("Game Speed", MENU_NUM, 10),
+        MenuPage("Debug mode", MENU_BOOL, 0)
+    })
+});
+
+bool inMenu = true;
+MenuHandler menu(&lcd, &menuRoot);
+
 byte chars[8] { };
 
+long t_last;
+
+byte dgCodes[10] { 22, 12, 24, 94, 8, 28, 90, 66, 82, 74 };
+
 void setup() {
-    menuRoot.setParents(true);
+    Serial.begin(9600);
     
     pinMode(PIN_IR, INPUT);
     pinMode(PIN_X, INPUT);
@@ -40,16 +52,69 @@ void setup() {
     lcd.clear();
 
     IR_begin(PIN_IR);
+
+    t_last = millis();
+
+    menuRoot.setParents(true);
+    menu.render();
 }
 
 const long frame_ms = 1000 / FPS;
-void loop() {
-    long t_last = millis();
+void loop() 
+{
+    long t_curr = millis();
+    if (t_curr - t_last >= frame_ms)
+    {
+        t_last = t_curr;
+        if (!inMenu) gameTick();
+    }
 
+    if (!IR_available()) { return; }
     
+    IrCmd cmd = IR_readCurrent();
 
-    long dt = millis() - t_last;
-    if (dt < frame_ms) delay(frame_ms - dt);
+    Serial.println(F("IR PACKET:"));
+    Serial.print(F("    Device ID : "));
+    Serial.println(cmd.deviceID);
+    Serial.print(F("    Command   : "));
+    Serial.println(cmd.command);
+    Serial.print(F("    Is repeat : "));
+    Serial.println(cmd.repeat);
+    Serial.print(F("    Error code: "));
+    Serial.println(cmd.error);
+
+    if (cmd.error || cmd.repeat) { return; }
+
+    if (inMenu)
+    {
+        if (cmd.command == 64 && menu.onSelect()) inMenu = false;
+        else if (cmd.command == 9) menu.onEscape();
+        else if (cmd.command == 25) menu.onReset();
+        else if (cmd.command == 67) menu.onMoveSubMenu(+1);
+        else if (cmd.command == 68) menu.onMoveSubMenu(-1);
+        else if (cmd.command == 13) menu.onDelete();
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (dgCodes[i] == cmd.command) 
+                {
+                    menu.onDigit(i);
+                    break;
+                }
+            }  
+        }   
+    }
+    else if (cmd.command == 64)
+    {
+        inMenu = true;
+        menu.setPage(&menuRoot);
+    }
+}
+
+void gameTick()
+{
+
 }
 
 void createChars()
